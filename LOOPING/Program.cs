09af18654f95031a3447200aefe2c7495cc99df1
Rasya Ofiyanto Saehan <110293838+xrsycode@users.noreply.github.com>
2023-11-19 06:09:54 +0700

    Add files via upload

---
+++ LOOPING/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Contoh_Looping
+{
+    static void Main()
+    {
+        Console.Clear();
+        int i;
+        i = 1; //nilai awal, preparation
+        Console.WriteLine("Contoh Latihan Looping dengan WHILE");
+        while (i <= 9) // nilai akhir, decision
+        {
+            Console.Write(i + " ");
+            i = i + 1;//counter, proses
+        }
+    }
+}
